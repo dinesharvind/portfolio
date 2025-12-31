d44e8f65f359be6e588269f1b8ffb6971cab3c49
dinesharvind <92197751+dinesharvind@users.noreply.github.com>
2025-12-31 12:02:55 +0530

    Update scrollingText.js

--- scrollingText.js
+++ scrollingText.js
@@ -9,5 +9,7 @@ window.addEventListener("scroll", () => {
     const scrollOffset = window.scrollY * 0.5; // Adjust the multiplier to control speed
     scrollingText.style.transform = `translateX(${scrollOffset}px)`;
     scrollingText2.style.transform = `translateX(${scrollOffset}px)`;
+    
 });
+
 
